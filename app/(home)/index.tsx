6f75052e0b5c1ad9d69cbf88379b01817204bc25
KotaroTaki <38860388+takikunxo@users.noreply.github.com>
2025-06-08 13:04:57 +0900

    ホーム画面のレイアウトとユーザー認証機能を新規作成。ユーザーのサインイン状態に応じて表示内容を切り替える機能を実装。

---
+++ app/(home)/index.tsx
@@ -0,0 +1,25 @@
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+import { SignOutButton } from "../component/SignOutButton";
+
+export default function Page() {
+  const { user } = useUser();
+
+  return (
+    <View>
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+        <SignOutButton />
+      </SignedIn>
+      <SignedOut>
+        <Link href="../(auth)/sign-in" asChild>
+          <Text>Sign in</Text>
+        </Link>
+        <Link href="../(auth)/sign-up" asChild>
+          <Text>Sign up</Text>
+        </Link>
+      </SignedOut>
+    </View>
+  );
+}
